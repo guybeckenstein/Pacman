021eb7fbdf2dca175dac28b856ca7070fb3c1e51
Guy Beckenstein <guybeckenstein@gmail.com>
2022-08-31 01:41:52 +0300

    Update Fruit.h

--- Pacman/Fruit.h
+++ Pacman/Fruit.h
@@ -12,22 +12,13 @@ private:
 	bool _isVisible;
 public:
 	// Constructors
-	Fruit(int movementFrequency = static_cast<int>(fruitConstants::MOVEMENT_FREQUENCY)) : Entity(movementFrequency), _number(static_cast<int>(Directions::TRASH)), _isVisible(false) {}
-	~Fruit() override = default;
+	Fruit(int movementFrequency = valOf(fruitConstants::MOVEMENT_FREQUENCY)) : Entity(movementFrequency), _number(valOf(pointConstants::TRASH)), _isVisible(false) {}
 	// Getters
 	int getNumber() const { return _number; }
 	bool getVisibility() const { return _isVisible; }
-<<<<<<< HEAD
-=======
-	// Setters
-	void setNumber() { _number = (rand() % static_cast<int>(fruitConstants::TOTAL_DIVISION_OPTIONS)) + static_cast<int>(fruitConstants::TOTAL_DIVISION_OPTIONS); }
-	void setNumber(int number) { _number = number; }
-	void setVisibility(bool value) { _isVisible = value; }
->>>>>>> f3a2e9a97bb00a5af3d781ff1083155583228d28
 	// Methods
 	void setStartingCoord(const Board& board, const vector<Point>& entities);
 	void makeFruitInvisibile();
-<<<<<<< HEAD
 	void Reset(const Board& board, const vector<Point>& entities);
 private:
 	// Setters
@@ -36,9 +27,3 @@ private:
 	void Render(std::ostream& os) const override { os << getNumber(); }
 	void setVisibility(bool value) { _isVisible = value; }
 };
-
-=======
-	void Reset(const Board& board, const vector<Point>& entities); // Not the same same as Entity::Reset
-	void Reset(const Point& coord) override {} // Entity::Reset - this line is unused but enables compilation
-};
->>>>>>> f3a2e9a97bb00a5af3d781ff1083155583228d28
